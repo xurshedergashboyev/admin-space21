8e1c68625707655fc6114c413bb177399e8df857
xurshedergashboyev <xurshedergashboyev@gmail.com>
2021-08-30 14:54:20 +0500

    drag and drop

--- src/app/modules/leads/components/SecondDrag.tsx
+++ src/app/modules/leads/components/SecondDrag.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import './drag.css'
 
 const data = [
   {
@@ -22,8 +23,10 @@ const data = [
 ]
 const SecondDrag = () => {
   const [boards, setBoards] = useState(data)
+  const [currentBoard, setCurrentBoard] = useState(null)
+  const [currentItem, setCurrentItem] = useState(null)
 
-  const dragOver = (e: any, board: any, i: any) => {
+  const dragOver = (e: any) => {
     e.preventDefault()
     if (e.target.className == 'item') {
       e.target.style.boxShadow = '0px 3px 3px gray '
@@ -32,23 +35,75 @@ const SecondDrag = () => {
   const dragLeave = (e: any) => {
     e.target.style.boxShadow = '0px 3px 3px gray '
   }
-  const dragStart = (e: any) => {}
+  const dragStart = (e: any, board: any, i: any) => {
+    setCurrentBoard(board)
+    setCurrentItem(i)
+  }
   const dragEnd = (e: any) => {
     e.target.style.boxShadow = '0px 3px 3px gray'
   }
-  const drop = (e: any, board: any, i: any) => {}
+  const drop = (e: any, board: any, i: any) => {
+    e.preventDefault()
+    // @ts-ignore
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    // @ts-ignore
+    currentBoard.items.splice(currentIndex, 1)
+    const dropIndex = board.items.indexOf(i)
+    board.items.splice(dropIndex + 1, 1, currentItem)
+    setBoards(
+      boards.map((b) => {
+        if (b.id === board.id) {
+          return board
+        } else {
+          // @ts-ignore
+          if (b.id === currentBoard.id) {
+            return currentBoard
+          }
+        }
+        return b
+      })
+    )
+  }
 
+  const dropBoard = (e: any, board: any) => {
+    board.items.push(currentItem)
+    // @ts-ignore
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    // @ts-ignore
+    currentBoard.items.splice(currentIndex, 1)
+    setBoards(
+      boards.map((b) => {
+        if (b.id === board.id) {
+          return board
+        } else {
+          // @ts-ignore
+          if (b.id === currentBoard.id) {
+            return currentBoard
+          }
+        }
+        return b
+      })
+    )
+  }
+
+  // @ts-ignore
   return (
     <>
       <div>
         {boards.map((board) => (
-          <div className={'board'}>
-            <div className={'board_title'}>{board.title}</div>
+          <div className={'flexbox'}>
+            <div
+              className={'board'}
+              onDragOver={(e) => dragOver(e)}
+              onDrop={(e) => dropBoard(e, board)}
+            >
+              {board.title}
+            </div>
             {board.items.map((i) => (
               <div
-                onDragOver={(e) => dragOver(e, board, i)}
+                onDragOver={(e) => dragOver(e)}
                 onDragLeave={(e) => dragLeave(e)}
-                onDragStart={(e) => dragStart(e)}
+                onDragStart={(e) => dragStart(e, board, i)}
                 onDragEnd={(e) => dragEnd(e)}
                 onDrop={(e) => drop(e, board, i)}
                 draggable={'true'}
--- src/app/modules/taskManagement/components/TaskItemsContainer.tsx
+++ src/app/modules/taskManagement/components/TaskItemsContainer.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react'
 import {TaskItemsColumn} from './TaskItemsColumn'
 import {TaskItemsProgress} from './TaskItemsProgress'
 import {TasksItem} from './TasksItem'
@@ -7,10 +7,94 @@ import {TaskUserIcon} from './TaskUserIcon'
 const users1: Array<TaskUserIcon> = [
   {name: 'Khurshed Ergashbaev', avatar: '/media/avatars/150-1.jpg'},
   {name: 'Muhammad Zakirov', avatar: '/media/avatars/150-2.jpg'},
-  {name: 'Maraim Tukhtasunov', initials: 'S', color: 'primary'}
+  {name: 'Maraim Tukhtasunov', initials: 'S', color: 'primary'},
+]
+
+const data = [
+  {
+    id: 1,
+    title: 'To Do',
+    items: [
+      {id: 1, title: 'Hello'},
+      {id: 2, title: 'Xurshed'},
+      {id: 3, title: 'Maraim'},
+    ],
+  },
+  {
+    id: 2,
+    title: 'In process',
+    items: [
+      {id: 4, title: 'Abdurshid'},
+      {id: 5, title: 'Bye Bye'},
+    ],
+  },
+  {id: 3, title: 'Done', items: [{id: 6, title: 'Drag'}]},
 ]
 
 const TaskItemsContainer: FC = () => {
+  const [boards, setBoards] = useState(data)
+  const [currentBoard, setCurrentBoard] = useState(null)
+  const [currentItem, setCurrentItem] = useState(null)
+
+  const dragOver = (e: any) => {
+    e.preventDefault()
+    if (e.target.className == 'item') {
+      e.target.style.boxShadow = '0px 3px 3px gray '
+    }
+  }
+  const dragLeave = (e: any) => {
+    e.target.style.boxShadow = '0px 3px 3px gray '
+  }
+  const dragStart = (e: any, board: any, i: any) => {
+    setCurrentBoard(board)
+    setCurrentItem(i)
+  }
+  const dragEnd = (e: any) => {
+    e.target.style.boxShadow = '0px 3px 3px gray'
+  }
+  const drop = (e: any, board: any, i: any) => {
+    e.preventDefault()
+    // @ts-ignore
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    // @ts-ignore
+    currentBoard.items.splice(currentIndex, 1)
+    const dropIndex = board.items.indexOf(i)
+    board.items.splice(dropIndex + 1, 1, currentItem)
+    setBoards(
+      boards.map((b) => {
+        if (b.id === board.id) {
+          return board
+        } else {
+          // @ts-ignore
+          if (b.id === currentBoard.id) {
+            return currentBoard
+          }
+        }
+        return b
+      })
+    )
+  }
+
+  const dropBoard = (e: any, board: any) => {
+    board.items.push(currentItem)
+    // @ts-ignore
+    const currentIndex = currentBoard.items.indexOf(currentItem)
+    // @ts-ignore
+    currentBoard.items.splice(currentIndex, 1)
+    setBoards(
+      boards.map((b) => {
+        if (b.id === board.id) {
+          return board
+        } else {
+          // @ts-ignore
+          if (b.id === currentBoard.id) {
+            return currentBoard
+          }
+        }
+        return b
+      })
+    )
+  }
   return (
     <>
       <div className='d-flex flex-wrap flex-stack mb-6'>
@@ -18,7 +102,6 @@ const TaskItemsContainer: FC = () => {
           My Projects
           <span className='fs-6 text-gray-400 fw-bold ms-1'>Active</span>
         </h3>
-
         <div className='d-flex flex-wrap my-2'>
           <div className='me-4'>
             <select
@@ -44,114 +127,235 @@ const TaskItemsContainer: FC = () => {
           </a>
         </div>
       </div>
-      <div className="d-flex justify-content-between">
-        <div className='row g-6 g-xl-9'>
-          <div className='col-md-10 col-xl-10'>
-            <TaskItemsProgress name={'Todo'} progress={'Todo'} borderColor={'danger'} />
-            <TasksItem
-              icon='/media/svg/brand-logos/plurk.svg'
-              badgeColor='primary'
-              status='In Progress'
-              statusColor='primary'
-              title='Fitnes App'
-              description='CRM App application to HR efficiency'
-              date='August 30, 2021'
-              budget='$284,900.00'
-              progress={"50"}
-              users={users1}
-            />
-            <TasksItem
-              icon='/media/svg/brand-logos/plurk.svg'
-              badgeColor='primary'
-              status='In Progress'
-              statusColor='primary'
-              title='Fitnes App'
-              description='CRM App application to HR efficiency'
-              date='August 30, 2021'
-              budget='$284,900.00'
-              progress={"50"}
-              users={users1}
-            />
-          </div>
-        </div>
-        <div className='row g-6 g-xl-9'>
-          <div className='col-md-6 col-xl-10'>
-            <TaskItemsProgress name={'Doing'} progress={'Doing'} borderColor={'primary'} />
-            <TasksItem
-              icon='/media/svg/brand-logos/plurk.svg'
-              badgeColor='primary'
-              status='In Progress'
-              statusColor='primary'
-              title='Fitnes App'
-              description='CRM App application to HR efficiency'
-              date='August 30, 2021'
-              budget='$284,900.00'
-              progress={"50"}
-              users={users1}
-            />
-            <TasksItem
-              icon='/media/svg/brand-logos/plurk.svg'
-              badgeColor='primary'
-              status='In Progress'
-              statusColor='primary'
-              title='Fitnes App'
-              description='CRM App application to HR efficiency'
-              date='August 30, 2021'
-              budget='$284,900.00'
-              progress={"50"}
-              users={users1}
-            />
-            <TasksItem
-              icon='/media/svg/brand-logos/plurk.svg'
-              badgeColor='primary'
-              status='In Progress'
-              statusColor='primary'
-              title='Fitnes App'
-              description='CRM App application to HR efficiency'
-              date='August 30, 2021'
-              budget='$284,900.00'
-              progress={"50"}
-              users={users1}
-            />
-          </div>
-        </div>
-        <div className='row g-6 g-xl-9'>
-          <div className='col-md-6 col-xl-10'>
-            <TaskItemsProgress name={'Testing'} progress={'Testing'} borderColor={'warning'} />
-            <TasksItem
-              icon='/media/svg/brand-logos/plurk.svg'
-              badgeColor='primary'
-              status='In Progress'
-              statusColor='primary'
-              title='Fitnes App'
-              description='CRM App application to HR efficiency'
-              date='August 30, 2021'
-              budget='$284,900.00'
-              progress={"50"}
-              users={users1}
-            />
-          </div>
-        </div>
-        <div className='row g-6 g-xl-9'>
-          <div className='col-md-6 col-xl-10'>
-            <TaskItemsProgress name={'Completed'} progress={'Completed'} borderColor={'success'} />
-            <TasksItem
-              icon='/media/svg/brand-logos/plurk.svg'
-              badgeColor='success'
-              status='Done'
-              statusColor='primary'
-              title='Fitnes App'
-              description='CRM App application to HR efficiency'
-              date='August 30, 2021'
-              budget='$284,900.00'
-              progress={"50"}
-              users={users1}
-            />
-          </div>
-        </div>
+      <div className='d-flex justify-content-between'>
+        {boards.map((board) => (
+          <>
+            <div className='row g-6 g-xl-9'>
+              <div
+                className='col-md-10 col-xl-10'
+                onDragOver={(e) => dragOver(e)}
+                onDrop={(e) => dropBoard(e, board)}
+              >
+                <TaskItemsProgress
+                  name={board.title}
+                  progress={board.title}
+                  borderColor={'danger'}
+                />
+                <TasksItem
+                  icon='/media/svg/brand-logos/plurk.svg'
+                  badgeColor='primary'
+                  status='In Progress'
+                  statusColor='primary'
+                  title='Fitnes App'
+                  description='CRM App application to HR efficiency'
+                  date='August 30, 2021'
+                  budget='$284,900.00'
+                  progress={'50'}
+                  users={users1}
+                />
+                <TasksItem
+                  icon='/media/svg/brand-logos/plurk.svg'
+                  badgeColor='primary'
+                  status='In Progress'
+                  statusColor='primary'
+                  title='Fitnes App'
+                  description='CRM App application to HR efficiency'
+                  date='August 30, 2021'
+                  budget='$284,900.00'
+                  progress={'50'}
+                  users={users1}
+                />
+              </div>
+            </div>
+            {/*<div className='row g-6 g-xl-9'>*/}
+            {/*  <div className='col-md-6 col-xl-10'>*/}
+            {/*    <TaskItemsProgress name={'Doing'} progress={'Doing'} borderColor={'primary'} />*/}
+            {/*    <TasksItem*/}
+            {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+            {/*      badgeColor='primary'*/}
+            {/*      status='In Progress'*/}
+            {/*      statusColor='primary'*/}
+            {/*      title='Fitnes App'*/}
+            {/*      description='CRM App application to HR efficiency'*/}
+            {/*      date='August 30, 2021'*/}
+            {/*      budget='$284,900.00'*/}
+            {/*      progress={'50'}*/}
+            {/*      users={users1}*/}
+            {/*    />*/}
+            {/*    <TasksItem*/}
+            {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+            {/*      badgeColor='primary'*/}
+            {/*      status='In Progress'*/}
+            {/*      statusColor='primary'*/}
+            {/*      title='Fitnes App'*/}
+            {/*      description='CRM App application to HR efficiency'*/}
+            {/*      date='August 30, 2021'*/}
+            {/*      budget='$284,900.00'*/}
+            {/*      progress={'50'}*/}
+            {/*      users={users1}*/}
+            {/*    />*/}
+            {/*    <TasksItem*/}
+            {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+            {/*      badgeColor='primary'*/}
+            {/*      status='In Progress'*/}
+            {/*      statusColor='primary'*/}
+            {/*      title='Fitnes App'*/}
+            {/*      description='CRM App application to HR efficiency'*/}
+            {/*      date='August 30, 2021'*/}
+            {/*      budget='$284,900.00'*/}
+            {/*      progress={'50'}*/}
+            {/*      users={users1}*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+            {/*<div className='row g-6 g-xl-9'>*/}
+            {/*  <div className='col-md-6 col-xl-10'>*/}
+            {/*    <TaskItemsProgress name={'Testing'} progress={'Testing'} borderColor={'warning'} />*/}
+            {/*    <TasksItem*/}
+            {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+            {/*      badgeColor='primary'*/}
+            {/*      status='In Progress'*/}
+            {/*      statusColor='primary'*/}
+            {/*      title='Fitnes App'*/}
+            {/*      description='CRM App application to HR efficiency'*/}
+            {/*      date='August 30, 2021'*/}
+            {/*      budget='$284,900.00'*/}
+            {/*      progress={'50'}*/}
+            {/*      users={users1}*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+            {/*<div className='row g-6 g-xl-9'>*/}
+            {/*  <div className='col-md-6 col-xl-10'>*/}
+            {/*    <TaskItemsProgress*/}
+            {/*      name={'Completed'}*/}
+            {/*      progress={'Completed'}*/}
+            {/*      borderColor={'success'}*/}
+            {/*    />*/}
+            {/*    <TasksItem*/}
+            {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+            {/*      badgeColor='success'*/}
+            {/*      status='Done'*/}
+            {/*      statusColor='primary'*/}
+            {/*      title='Fitnes App'*/}
+            {/*      description='CRM App application to HR efficiency'*/}
+            {/*      date='August 30, 2021'*/}
+            {/*      budget='$284,900.00'*/}
+            {/*      progress={'50'}*/}
+            {/*      users={users1}*/}
+            {/*    />*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+          </>
+        ))}
+        {/*<div className='row g-6 g-xl-9'>*/}
+        {/*  <div className='col-md-10 col-xl-10' onDragOver={(e) => dragOver(e)}*/}
+        {/*       onDrop={(e) => dropBoard(e, board)}>*/}
+        {/*    <TaskItemsProgress name={'Todo'} progress={'Todo'} borderColor={'danger'} />*/}
+        {/*    <TasksItem*/}
+        {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+        {/*      badgeColor='primary'*/}
+        {/*      status='In Progress'*/}
+        {/*      statusColor='primary'*/}
+        {/*      title='Fitnes App'*/}
+        {/*      description='CRM App application to HR efficiency'*/}
+        {/*      date='August 30, 2021'*/}
+        {/*      budget='$284,900.00'*/}
+        {/*      progress={'50'}*/}
+        {/*      users={users1}*/}
+        {/*    />*/}
+        {/*    <TasksItem*/}
+        {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+        {/*      badgeColor='primary'*/}
+        {/*      status='In Progress'*/}
+        {/*      statusColor='primary'*/}
+        {/*      title='Fitnes App'*/}
+        {/*      description='CRM App application to HR efficiency'*/}
+        {/*      date='August 30, 2021'*/}
+        {/*      budget='$284,900.00'*/}
+        {/*      progress={'50'}*/}
+        {/*      users={users1}*/}
+        {/*    />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+        {/*<div className='row g-6 g-xl-9'>*/}
+        {/*  <div className='col-md-6 col-xl-10'>*/}
+        {/*    <TaskItemsProgress name={'Doing'} progress={'Doing'} borderColor={'primary'} />*/}
+        {/*    <TasksItem*/}
+        {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+        {/*      badgeColor='primary'*/}
+        {/*      status='In Progress'*/}
+        {/*      statusColor='primary'*/}
+        {/*      title='Fitnes App'*/}
+        {/*      description='CRM App application to HR efficiency'*/}
+        {/*      date='August 30, 2021'*/}
+        {/*      budget='$284,900.00'*/}
+        {/*      progress={'50'}*/}
+        {/*      users={users1}*/}
+        {/*    />*/}
+        {/*    <TasksItem*/}
+        {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+        {/*      badgeColor='primary'*/}
+        {/*      status='In Progress'*/}
+        {/*      statusColor='primary'*/}
+        {/*      title='Fitnes App'*/}
+        {/*      description='CRM App application to HR efficiency'*/}
+        {/*      date='August 30, 2021'*/}
+        {/*      budget='$284,900.00'*/}
+        {/*      progress={'50'}*/}
+        {/*      users={users1}*/}
+        {/*    />*/}
+        {/*    <TasksItem*/}
+        {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+        {/*      badgeColor='primary'*/}
+        {/*      status='In Progress'*/}
+        {/*      statusColor='primary'*/}
+        {/*      title='Fitnes App'*/}
+        {/*      description='CRM App application to HR efficiency'*/}
+        {/*      date='August 30, 2021'*/}
+        {/*      budget='$284,900.00'*/}
+        {/*      progress={'50'}*/}
+        {/*      users={users1}*/}
+        {/*    />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+        {/*<div className='row g-6 g-xl-9'>*/}
+        {/*  <div className='col-md-6 col-xl-10'>*/}
+        {/*    <TaskItemsProgress name={'Testing'} progress={'Testing'} borderColor={'warning'} />*/}
+        {/*    <TasksItem*/}
+        {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+        {/*      badgeColor='primary'*/}
+        {/*      status='In Progress'*/}
+        {/*      statusColor='primary'*/}
+        {/*      title='Fitnes App'*/}
+        {/*      description='CRM App application to HR efficiency'*/}
+        {/*      date='August 30, 2021'*/}
+        {/*      budget='$284,900.00'*/}
+        {/*      progress={'50'}*/}
+        {/*      users={users1}*/}
+        {/*    />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+        {/*<div className='row g-6 g-xl-9'>*/}
+        {/*  <div className='col-md-6 col-xl-10'>*/}
+        {/*    <TaskItemsProgress name={'Completed'} progress={'Completed'} borderColor={'success'} />*/}
+        {/*    <TasksItem*/}
+        {/*      icon='/media/svg/brand-logos/plurk.svg'*/}
+        {/*      badgeColor='success'*/}
+        {/*      status='Done'*/}
+        {/*      statusColor='primary'*/}
+        {/*      title='Fitnes App'*/}
+        {/*      description='CRM App application to HR efficiency'*/}
+        {/*      date='August 30, 2021'*/}
+        {/*      budget='$284,900.00'*/}
+        {/*      progress={'50'}*/}
+        {/*      users={users1}*/}
+        {/*    />*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </>
   )
 }
 
-export {TaskItemsContainer};
+export {TaskItemsContainer}
