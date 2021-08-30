13fbf551b1e546d65a862862fd3601e5038fc79d
xurshedergashboyev <xurshedergashboyev@gmail.com>
2021-08-30 18:04:23 +0500

    drag and drop

--- src/_metronic/layout/components/aside/AsideMenuMain.tsx
+++ src/_metronic/layout/components/aside/AsideMenuMain.tsx
@@ -109,19 +109,19 @@ export function AsideMenuMain() {
       {/*  <AsideMenuItem to='/error/404' title='Error 404' hasBullet={true} />*/}
       {/*  <AsideMenuItem to='/error/500' title='Error 500' hasBullet={true} />*/}
       {/*</AsideMenuItemWithSub>*/}
-      <AsideMenuItemWithSub
-        to='/crafted/widgets'
-        title='Widgets'
-        icon='/media/icons/duotone/Layout/Layout-4-blocks.svg'
-        fontIcon='bi-layers'
-      >
-        <AsideMenuItem to='/crafted/widgets/lists' title='Lists' hasBullet={true} />
-        <AsideMenuItem to='/crafted/widgets/statistics' title='Statistics' hasBullet={true} />
-        <AsideMenuItem to='/crafted/widgets/charts' title='Charts' hasBullet={true} />
-        <AsideMenuItem to='/crafted/widgets/mixed' title='Mixed' hasBullet={true} />
-        <AsideMenuItem to='/crafted/widgets/tables' title='Tables' hasBullet={true} />
-        <AsideMenuItem to='/crafted/widgets/feeds' title='Feeds' hasBullet={true} />
-      </AsideMenuItemWithSub>
+      {/*<AsideMenuItemWithSub*/}
+      {/*  to='/crafted/widgets'*/}
+      {/*  title='Widgets'*/}
+      {/*  icon='/media/icons/duotone/Layout/Layout-4-blocks.svg'*/}
+      {/*  fontIcon='bi-layers'*/}
+      {/*>*/}
+      {/*  <AsideMenuItem to='/crafted/widgets/lists' title='Lists' hasBullet={true} />*/}
+      {/*  <AsideMenuItem to='/crafted/widgets/statistics' title='Statistics' hasBullet={true} />*/}
+      {/*  <AsideMenuItem to='/crafted/widgets/charts' title='Charts' hasBullet={true} />*/}
+      {/*  <AsideMenuItem to='/crafted/widgets/mixed' title='Mixed' hasBullet={true} />*/}
+      {/*  <AsideMenuItem to='/crafted/widgets/tables' title='Tables' hasBullet={true} />*/}
+      {/*  <AsideMenuItem to='/crafted/widgets/feeds' title='Feeds' hasBullet={true} />*/}
+      {/*</AsideMenuItemWithSub>*/}
       <div className='menu-item'>
         <div className='menu-content pt-8 pb-2'>
           <span className='menu-section text-muted text-uppercase fs-8 ls-1'>Apps</span>
