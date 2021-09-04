a11b6a71e3f0e9f8e341fd142d53f87dce736e4b
xurshedergashboyev <xurshedergashboyev@gmail.com>
2021-09-04 10:36:35 +0500

    creating modal

--- src/app/modules/taskManagement/components/TaskItemsContainer.tsx
+++ src/app/modules/taskManagement/components/TaskItemsContainer.tsx
@@ -197,8 +197,9 @@ const TaskItemsContainer: FC = () => {
                                 className='bi-textarea-resize w-50 fs-1 p-5'
                               />
                             </div>
-
-                            <h3 className='fs-1 my-5'>To do</h3>
+                            <div className='d-flex align-items-center'>
+                              <h3 className='fs-1 my-5'>To do</h3>
+                            </div>
                           </div>
                         </div>
                       </div>
