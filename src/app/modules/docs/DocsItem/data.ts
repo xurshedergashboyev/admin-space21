9c7d87218eb1c3ebe579536a801abeb818ce8a43
vladyslavCodeCare <vladyslav.s@code-care.pro>
2021-10-10 16:29:34 +0500

    docs page

---
+++ src/app/modules/docs/DocsItem/data.ts
@@ -0,0 +1,44 @@
+interface Item {
+  id: number
+  title: string
+  progress: string
+}
+
+interface Data {
+  id: number
+  title: string
+  color: string
+  items: Item[]
+}
+
+export const data: Data[] = [
+  {
+    id: 1,
+    title: 'To Do',
+    color: 'danger',
+    items: [
+      {id: 1, title: 'Hello', progress: 'To do'},
+      {id: 2, title: 'Xurshed', progress: 'To do'},
+      {id: 3, title: 'Maraim', progress: 'To do'},
+    ],
+  },
+  {
+    id: 2,
+    title: 'Doing',
+    color: 'primary',
+    items: [
+      {id: 4, title: 'Abdurashid', progress: 'Doing'},
+      {id: 5, title: 'Bye Bye', progress: 'Doing'},
+    ],
+  },
+  {id: 3, title: 'Review', color: 'warning', items: [{id: 6, title: 'Drag', progress: 'Review'}]},
+  {
+    id: 4,
+    title: 'Done',
+    color: 'success',
+    items: [
+      {id: 7, title: 'Create a website', progress: 'Done'},
+      {id: 8, title: 'Telegram Bot', progress: 'Done'},
+    ],
+  },
+]
--- src/app/modules/docs/DocsItem/index.tsx
+++ src/app/modules/docs/DocsItem/index.tsx
@@ -1,11 +1,108 @@
 import React, {FC} from 'react'
-import {toAbsoluteUrl} from '../../../../_metronic/helpers'
+import {KTSVG, toAbsoluteUrl} from '../../../../_metronic/helpers'
 import {TaskUserIcon} from '../../taskManagement/components/TaskUserIcon'
+import {TaskUsers} from '../../taskManagement/components/TaskUsers'
 
 const DocsItem: FC = () => {
   return (
     <>
-      <h1>Hello</h1>
+      <div
+        className='card border border-2 border-gray-300 border-hover mb-5'
+        data-bs-target='#exampleModalToggle2'
+        data-bs-toggle='modal'
+        data-bs-dismiss='modal'
+      >
+        {/*<div*/}
+        {/*  className='modal fade'*/}
+        {/*  id='exampleModalToggle2'*/}
+        {/*  aria-hidden='true'*/}
+        {/*  aria-labelledby='exampleModalToggleLabel2'*/}
+        {/*>*/}
+        {/*  <div className='modal-dialog modal-fullscreen'>*/}
+        {/*    <div className='modal-content'>*/}
+        {/*      <div className='modal-header'>*/}
+        {/*        <h5 className='modal-title' id='exampleModalToggleLabel2'>*/}
+        {/*          {title} {id}*/}
+        {/*        </h5>*/}
+        {/*        <button className={`btn btn-${categoryColor} mx-5`}>{category}</button>*/}
+        {/*        <TaskUsers users={users} />*/}
+        {/*        <OverlayTrigger*/}
+        {/*          key={status}*/}
+        {/*          overlay={<Tooltip id={'tooltip-user-name'}>{status}</Tooltip>}*/}
+        {/*          placement={'bottom'}*/}
+        {/*        >*/}
+        {/*          <div className='symbol symbol-35px symbol-circle border-danger border mx-5'>*/}
+        {/*            <KTSVG*/}
+        {/*              path='/media/icons/duotone/Design/Target.svg'*/}
+        {/*              className='svg-icon-2hx svg-icon-danger'*/}
+        {/*            />*/}
+        {/*          </div>*/}
+        {/*        </OverlayTrigger>*/}
+        {/*        <button*/}
+        {/*          type='button'*/}
+        {/*          className='btn-close'*/}
+        {/*          data-bs-dismiss='modal'*/}
+        {/*          aria-label='Close'*/}
+        {/*        />*/}
+        {/*      </div>*/}
+        {/*      <div className='modal-body'>*/}
+        {/*        <textarea*/}
+        {/*          className='bi-textarea-resize w-50 fs-1 p-5'*/}
+        {/*          value={title}*/}
+        {/*          onChange={(e) => e.target.value}*/}
+        {/*        />*/}
+        {/*        <div id='toolbar' />*/}
+
+        {/*        <div id='editor'>*/}
+        {/*          <textarea*/}
+        {/*            value={description}*/}
+        {/*            onChange={(e) => e.target.value}*/}
+        {/*            className='bi-textarea-resize w-50 fs-1 p-5'*/}
+        {/*          />*/}
+        {/*        </div>*/}
+        {/*      </div>*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
+        <div className='card-header border-0'>
+          <div className='card-title m-0'>
+            <div className='symbol symbol-30px w-30px bg-light'>
+              {/*<img src={toAbsoluteUrl('')} alt='card2' className='p-3' />*/}
+            </div>
+          </div>
+          <div className='card-toolbar'>
+            <span className={`badge badge-light-primary fw-bolder`}>fsdaf</span>
+            <span className={`badge badge-light-primary fw-bolder mx-2 cursor-pointer`}>
+              <KTSVG path='/media/icons/duotone/General/Edit.svg' className='svg-icon-1hx' />
+            </span>
+          </div>
+        </div>
+
+        <div className='card-body px-9 pt-0'>
+          <div className='fs-5 fw-bolder text-dark'>Hello</div>
+          <p className='text-gray-400 fw-bold fs-7 mt-1 mb-7'>Lorem ipsum dolor sit amet.</p>
+          {/*<div className='d-flex flex-wrap mb-5'>*/}
+          {/*  <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 me-7 mb-3'>*/}
+          {/*    <div className='fs-7 text-gray-800 fw-bolder'>{date}</div>*/}
+          {/*    <div className='fw-bold text-gray-400'>Due Date</div>*/}
+          {/*  </div>*/}
+
+          {/*  <div className='border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 mb-3'>*/}
+          {/*    <div className='fs-6 text-gray-800 fw-bolder'>{budget}</div>*/}
+          {/*    <div className='fw-bold text-gray-400'>Budget</div>*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+
+          <div
+            className='h-4px w-100 bg-light mb-5'
+            data-bs-toggle='tooltip'
+            title='This project completed'
+          >
+            <div className={`bg-primary rounded h-4px`} role='progressbar' style={{width: `54%`}} />
+          </div>
+          {/*<TaskUsers users={users} />*/}
+        </div>
+      </div>
     </>
   )
 }
