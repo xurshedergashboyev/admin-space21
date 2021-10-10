5b229fbf8d7b6c5c9923e7b767da420e4b0f4e85
vladyslavCodeCare <vladyslav.s@code-care.pro>
2021-10-10 18:23:36 +0500

    integration

--- src/app/modules/integration/IntegrationItem/index.tsx
+++ src/app/modules/integration/IntegrationItem/index.tsx
@@ -9,63 +9,63 @@ interface Items {
 
 const data: Items[] = [
   {
-    img: '/media/technology/Angular.png',
+    img: '/media/technology-logos/Angular.png',
     connected: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/Html5.png',
+    img: '/media/technology-logos/Html5.png',
     connected: true,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/React.png',
+    img: '/media/technology-logos/React.png',
     connected: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/VueJS.png',
+    img: '/media/technology-logos/VueJS.png',
     connected: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/Html5.png',
+    img: '/media/technology-logos/Html5.png',
     connected: true,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/React.png',
+    img: '/media/technology-logos/React.png',
     connected: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/Angular.png',
+    img: '/media/technology-logos/Angular.png',
     connected: true,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/Html5.png',
+    img: '/media/technology-logos/Html5.png',
     connected: false,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
       '            dolore hic impedit itaque perspiciatis quaerat quos totam.',
   },
   {
-    img: '/media/technology/React.png',
+    img: '/media/technology-logos/React.png',
     connected: true,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet beatae cumque deleniti\n' +
@@ -75,17 +75,17 @@ const data: Items[] = [
 
 const IntegrationItem: FC = () => {
   return (
-    <div className={'d-flex justify-content-center'}>
+    <div className={'d-flex flex-wrap justify-content-center'}>
       {data.map((value) => (
-        <div className={'card  mx-5 p-5'}>
+        <div className={'card col-3 mx-5 p-5 my-5'}>
           <div className={'d-flex justify-content-between align-items-center'}>
             <img
               // className={'w-10 h-10'}
               src={toAbsoluteUrl(value.img)}
               alt='angular'
             />
-            <button className={`btn ${value.connected ? 'btn-success' : 'btn-light'}btn-sm`}>
-              {value.img ? 'Connected' : 'Connect'}
+            <button className={`btn ${value.connected ? 'btn-success' : 'btn-light'} btn-sm`}>
+              {value.connected ? 'Connected' : 'Connect'}
             </button>
           </div>
           <div className={'my-5'}>
